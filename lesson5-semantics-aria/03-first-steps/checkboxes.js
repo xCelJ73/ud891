92736a7e614b0bf9532fde7b6941e065e8c27229
xCelJ73 <36142151+xCelJ73@users.noreply.github.com>
2018-10-18 18:15:47 +0800

    Update checkboxes.js

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -21,6 +21,11 @@
     this.el.addEventListener('click', this.toggle.bind(this));
 
     // Any other set-up we want to do here?
+    this.el.setAttribute('role', 'checkbox');
+    if (this.el.hasAttribute('checked')) {
+    this.el.setAttribute('aria-checked','true');
+    } else { this.el.setAttribue('aria-checked', 'false');
+    }
   }
 
   Checkbox.prototype.handleKeyDown = function(e) {
@@ -38,12 +43,12 @@
       this.el.removeAttribute('checked');
 
       // Hmm.
-
+   this.el.setAttribue('aria-checked', 'false');
     } else {
       this.el.setAttribute('checked', '');
 
       // Hmmmmm.
-
+     this.el.setAttribute('aria-checked','true');
     }
   };
 
